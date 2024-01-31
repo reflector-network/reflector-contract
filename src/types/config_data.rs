ce74567af02e4b8feccbed6347fe48b6677c604f
hawthorne-abendsen <hawthorne@stellar.expert>
2024-02-01 00:17:42 +0200

    replaced doc comments with regular ones to reduce wasm size

--- src/types/config_data.rs
+++ src/types/config_data.rs
@@ -5,18 +5,18 @@ use super::asset::Asset;
 #[contracttype]
 #[derive(Clone, Debug, Eq, PartialEq)]
 
-/// The configuration parameters for the contract.
+// The configuration parameters for the contract.
 pub struct ConfigData {
-    /// The admin address.
+    // The admin address.
     pub admin: Address,
-    /// The retention period for the prices.
+    // The retention period for the prices.
     pub period: u64,
-    /// The assets supported by the contract.
+    // The assets supported by the contract.
     pub assets: Vec<Asset>,
-    /// The base asset for the prices.
+    // The base asset for the prices.
     pub base_asset: Asset,
-    /// The number of decimals for the prices.
+    // The number of decimals for the prices.
     pub decimals: u32,
-    /// The resolution of the prices.
+    // The resolution of the prices.
     pub resolution: u32
 }
--- src/types/error.rs
+++ src/types/error.rs
@@ -2,22 +2,22 @@ use soroban_sdk::contracterror;
 
 #[contracterror]
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
-/// The error codes for the contract.
+// The error codes for the contract.
 pub enum Error {
-    /// The contract is already initialized.
+    // The contract is already initialized.
     AlreadyInitialized = 0,
-    /// The caller is not authorized to perform the operation.
+    // The caller is not authorized to perform the operation.
     Unauthorized = 1,
-    /// The config assets doen't contain persistent asset. Delete assets is not supported.
+    // The config assets doen't contain persistent asset. Delete assets is not supported.
     AssetMissing = 2,
-    /// The asset is already added to the contract's list of supported assets.
+    // The asset is already added to the contract's list of supported assets.
     AssetAlreadyExists = 3,
-    /// The config version is invalid
+    // The config version is invalid
     InvalidConfigVersion = 4,
-    /// The prices timestamp is invalid
+    // The prices timestamp is invalid
     InvalidTimestamp = 5,
-    /// The assets update length or prices update length is invalid
+    // The assets update length or prices update length is invalid
     InvalidUpdateLength = 6,
-    /// The assets storage is full
+    // The assets storage is full
     AssetLimitExceeded = 7
 }
